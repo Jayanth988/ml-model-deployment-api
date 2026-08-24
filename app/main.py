f952be9163d5e28302b8eff9b64cd559ca2b4d0b
Jayanth Jawalkar <jayanthjawalkar@gmail.com>
2026-08-24 10:55:52 +0530

    Task 5 completed - integrated ML model with FastAPI

--- app/main.py
+++ app/main.py
@@ -1,11 +1,39 @@
 from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import joblib
 
-app = FastAPI()
+model = None
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global model
+
+    print("Loading Model...")
+
+    model = joblib.load("ml/saved_model/model.joblib")
+
+    print("Model Loaded Successfully")
+
+    yield
+
+    print("Application Shutdown")
+
+
+app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 def root():
     return {"message": "ML API is alive"}
 
+
 @app.post("/predict")
 def predict():
-    return {"prediction": "hardcoded_result"}
+    features = [5.1, 3.5, 1.4, 0.2]
+
+    prediction = model.predict([features])
+
+    return {
+        "prediction": int(prediction[0])
+    }
